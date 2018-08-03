a38e84e13ccc95637650bfb5444fcd81df1afbaf
Dima <dserp18@gmail.com>
2018-08-03 11:37:09 +0300

    Update series_sum.rkt

--- series_sum.rkt
+++ series_sum.rkt
@@ -8,3 +8,13 @@
 (series_sum 100)
 (series_sum 1000)
 (series_sum 10000)
+
+(define series_sum_1
+ ( lambda (n)
+  (if (= n 0) 0 
+    (+ (/ 1.0 (* n (+ n 1.0))) (series_sum_1(- n 1.0)))
+  ) ) )
+(series_sum_1 10)
+(series_sum_1 100)
+(series_sum_1 1000)
+(series_sum_1 10000)
