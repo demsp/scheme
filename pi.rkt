d25c0d035103d27e20fb7b18ff5843f9c501b948
demsp <dserp18@gmail.com>
2019-03-08 00:35:00 +0300

    Create pi.rkt

---
+++ pi.rkt
@@ -0,0 +1,17 @@
+; sicp. page 78
+
+(define (sum term a next b)
+(if (> a b)
+0
+(+ (term a)
+(sum term (next a) next b))))
+
+(define (pi-sum a b)
+(define (pi-term x)
+(/ 1.0 (* x (+ x 2))))
+(define (pi-next x)
+(+ x 4))
+(sum pi-term a pi-next b))
+
+(* 8 (pi-sum 1 1000))
+;3.139592655589783
