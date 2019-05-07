547e44cdde94663a4ee38c54f8f9782e208a08d7
demsp <dserp18@gmail.com>
2019-05-07 12:45:55 +0300

    Create main.hs

---
+++ main.hs
@@ -0,0 +1,20 @@
+--можно заменить Integer на Int
+x :: Integer
+y :: Integer
+x = 5
+y = 1
+mystr :: String
+mystr="lol"
+--function definition 
+add :: Integer -> Integer -> Integer   
+add x y =  x + y                       
+
+main = do
+  putStrLn "Hello"
+  putStrLn "World"
+  putStrLn mystr --putStrLn $ mystr
+  --calling a function 
+  print(add 2 5)
+  print(add x y)    
+  
+
